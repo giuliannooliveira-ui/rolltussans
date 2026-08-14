9731979ad44975aef3e7acad67fa7863cba90b88
giuliannooliveira-ui <giulianno.oliveira@segurosunimed.com.br>
2026-08-13 22:43:02 -0300

    Fix GitHub URL for TUSS_ANS.xlsx

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 )
 
 # Coloque a sua URL Raw do GitHub aqui
-URL_PLANILHA_GITHUB = "https://raw.githubusercontent.com/giuliannooliveira-ui/rolltussans/main/downloads/TUSS_ANS.xlsx.xlsx"
+URL_PLANILHA_GITHUB = "https://raw.githubusercontent.com/giuliannooliveira-ui/rolltussans/main/downloads/TUSS_ANS.xlsx"
 
 @app.get("/")
 def home():
